5b66a27a9bf669869a3fab1c78826d8ee47d9d32
AlltoaCode <unconfigured@null.spigotmc.org>
2021-02-02 16:59:37 +0100

    TEST 2 pořád nevím jak git funguje

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://en.wikipedia.org/wiki/Main_Page')
-print(r.status_code)
-print(r.json())
+r = requests.get('https://www.python.org')
+print(int(r.status_code))
+
+soup = BeautifulSoup(r.text, 'html.parser')
+soup = soup.prettify()
+print(soup)
+
+soup.
