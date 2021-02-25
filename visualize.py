8e286857c5d72c508b78c1fbd796f7d5d01ee3ae
Alltoa <wenclm@seznam.cz>
2021-02-25 23:46:23 +0100

    Added comments and cleared up.

--- visualize.py
+++ visualize.py
@@ -1,27 +1,34 @@
 import networkx as nx 
 import matplotlib.pyplot as plt 
    
-  
-class GraphVisualization: 
-   
+# GRAPH CLASS
+class GraphSimplify: 
+    """
+    used to simplify the way of adding edges to the networkx graph, first saving them internally, then passing them all at once with the call of visualize().
+    """
+
     def __init__(self): 
-        self.visual = [] 
+        self.edges = [] 
           
     def addEdge(self, a, b): 
         temp = [a, b] 
-        self.visual.append(temp) 
+        self.edges.append(temp) 
 
     def visualize(self): 
         G = nx.MultiGraph() 
-        G.add_edges_from(self.visual) 
+        G.add_edges_from(self.edges) 
         nx.draw_networkx(G) 
         plt.show() 
   
+# FUNCTIONS
+def draw_graph(set_of_WebPage_objs):
+    """
+    Function used for drawing the Graph itself, takes a set of WebPage objects, adds egdes between each object URL and linked URLs, then calls visualize() to draw the graph.
+    """
 
-def draw_graph(list_of_WebPage_objs):
-    G = GraphVisualization() 
+    G = GraphSimplify() 
 
-    for webpage in list_of_WebPage_objs:
+    for webpage in set_of_WebPage_objs:
         for linked_page in webpage.working_pages:
             G.addEdge(webpage.url,linked_page)
 
